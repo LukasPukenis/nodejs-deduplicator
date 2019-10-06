3dc0d35e1eecdeff8a045fe59d398273d451c2b0
Lukas Pukenis <lukas.pukenis@gmail.com>
2019-10-06 10:21:28 +0300

    split abort handler into functions

--- src/deduplicator.ts
+++ src/deduplicator.ts
@@ -29,8 +29,10 @@ export class Deduplicator {
     private verbose: boolean;
     private bytesRead: [number, number];
     private lineReader: ReadLine;
+    // closing file stream doesnt immediately stop it from firing events so we must use a flag alongside
     private aborted: boolean;
     private types: string[];
+
     constructor(private path: string, resultPath: string, types: string) {
         if (types) {
             this.types = types.split(',').map( (type: string) => '.' + type);
@@ -40,55 +42,21 @@ export class Deduplicator {
         /**
          * cleanup is called even when app exists correctly just with exit code of 0
          * so in case it's normal exit, just cleanup and that's it
-         * in case app is killed we write the previous read byte count into a lock file
+         * in case app is killed/aborted we write the previous read byte count into a lock file
          * to enable resume in the future
          */
         nodeCleanup((exitCode: number, signal: number) => {
             if (exitCode !== 0 || exitCode == null) {
-                if (this.verbose) {
-                    console.log(chalk.red('Aborting...'));
-                }
-
-                this.aborted = true;
-                if (this.lineReader) {
-                    this.lineReader.close();
-                }
-
-                if (this.verbose) {
-                    console.log("Writing lock file", this.bytesRead[0]);
-                }
-                fs.writeFileSync(LOCK_FILE, this.bytesRead[0]);
-
-                process.kill(process.pid, signal);
-                nodeCleanup.uninstall();
-                return false;
+                this.cleanupAbort(signal);
             } else {
-
-                console.log("Cleaning up");
-                if (fs.existsSync(LOCK_FILE)) {
-                    if (this.verbose) {
-                        console.log("Removing lockfile");
-                    }
-
-                    fs.unlinkSync(LOCK_FILE);
-                }
-
-                if (fs.existsSync(FILE_LIST)) {
-                    if (this.verbose) {
-                        console.log("Removing listfile");
-                    }
-
-                    fs.unlinkSync(FILE_LIST);
-                }
-                process.kill(process.pid, signal);
-                nodeCleanup.uninstall();
-
-                console.log("Exiting");
-                return false;
+                this.cleanupSuccess(signal);
             }
+
+            return false;
         });
 
         console.assert(MAX_CONCURRENT_HASHES > 0, "Max concurrent hash calculations must be greater than zero");
+
 
         this.resultLogger = new FileLogger(resultPath);
         // this.resultLogger = new ConsoleLogger();
@@ -104,16 +72,17 @@ export class Deduplicator {
         try {
             if (fs.existsSync(FILE_LIST)) {
                 console.log("File list was found");
-                const resume = await readlineSync.keyInYN("Resume operation or start fresh?");
+                const resume = await readlineSync.keyInYN("Resume operation(Y) or start fresh(N)?");
 
                 if (resume) {
                     const exists = fs.existsSync(LOCK_FILE);
                     console.assert(exists, "Lock file must also exist alongside file list");
-                    if (!exists) {
-                        process.exit(2);
-                    }
 
-                    await this._resume();
+                    if (exists) {
+                        await this._resume();
+                    } else {
+                        await this._process();
+                    }
                 } else {
                     await this._process();
                 }
@@ -122,7 +91,7 @@ export class Deduplicator {
                 await this._process();
             }
         } catch(e) {
-            console.log(e);
+            console.error(chalk.red('Error: ', e));
             return false;
         }
 
@@ -171,7 +140,7 @@ export class Deduplicator {
 
             return this._process(lastIndex);
         } catch(e) {
-            console.error("Error while resuming:", e);
+            console.error(chalk.red("Error while resuming:", e));
         }
     }
 
@@ -274,5 +243,48 @@ export class Deduplicator {
         }
 
         return promise;
+    }
+
+    private cleanupSuccess(signal: number) {
+        if (this.verbose) {
+            console.log("Cleaning up after success");
+        }
+
+        if (fs.existsSync(LOCK_FILE)) {
+            if (this.verbose) {
+                console.log("Removing lockfile");
+            }
+
+            fs.unlinkSync(LOCK_FILE);
+        }
+
+        if (fs.existsSync(FILE_LIST)) {
+            if (this.verbose) {
+                console.log("Removing listfile");
+            }
+
+            fs.unlinkSync(FILE_LIST);
+        }
+        process.kill(process.pid, signal);
+        nodeCleanup.uninstall();
+
+        console.log("Exiting");
+    }
+
+    private cleanupAbort(signal: number) {
+        console.log(chalk.red('Aborting...'));
+
+        this.aborted = true;
+        if (this.lineReader) {
+            this.lineReader.close();
+        }
+
+        if (this.verbose) {
+            console.log("Writing lock file", this.bytesRead[0]);
+        }
+        fs.writeFileSync(LOCK_FILE, this.bytesRead[0]);
+
+        process.kill(process.pid, signal);
+        nodeCleanup.uninstall();
     }
 }
